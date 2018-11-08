de652c4af49be598589d38f2e284a9ed5344294a
Craig Jones <jonescr3@gmail.com>
2018-11-08 10:06:19 -0500

    Play with styled components

--- crajola/src/App.js
+++ crajola/src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Splash from './imports/Splash.jsx';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to Crajola.</p>
-        </header>
-      </div>
-    );
+    return <Splash />;
   }
 }
 
